54138be7ceafedab222800a606007c1a1bf79705
destinfloyd <destin.b.floyd@gmail.com>
2019-04-03 09:21:54 -0400

    cant delete language if its a refrenceed FK.

--- penpal_app/models.py
+++ penpal_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Avg
 
 # Create your models here.
 class User(models.Model):
@@ -14,27 +15,42 @@ class User(models.Model):
 class Language(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    averageProficiency = models.IntegerField(default=0)
-    def __str__(self):
-        return (self.name)
+    averageProficiency = models.IntegerField(null=True, blank=True, default=None)
 
-class Message(models.Model):
-    id = models.AutoField(primary_key=True)
-    fromUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messagesSent")
-    toUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messagesRecieved")
-    language = models.ForeignKey(Language, on_delete=models.CASCADE, related_name="messagesLanguage")
-    text =  models.TextField()
     def __str__(self):
-        return (self.text)
+        return (self.name)   
+    
+    # averageProficiency = Proficiency.objects.filter(language=id).aggregate(Avg('level')).values()[0]
 
 class Proficiency(models.Model):
     id = models.AutoField(primary_key=True)
     level = models.IntegerField(choices=list(zip(range(1, 11), range(1, 11))), unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users")
-    language = models.ForeignKey(Language, on_delete=models.CASCADE, related_name="language")
-    # def __str__(self):
-    #     return (self.)
+    language = models.ForeignKey(Language, on_delete=models.PROTECT, related_name="language")
+    
 
-# make lang name = unique and make prof FK the name of LANG 
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    fromUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messagesSent")
+    toUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messagesRecieved")
+    language = models.ForeignKey(Language, on_delete=models.PROTECT, related_name="messagesLanguage")
+    text =  models.TextField()
+    def __str__(self):
+        return (self.text)
+
+    def save(self, *args, **kwargs):
+        if self.fromUser 
+        # its not the same lang with same level w/in2
+        # https://docs.djangoproject.com/en/2.2/topics/db/models/#field-name-restrictions
+            return # Not a vaild message due to language!
+# forms validation 
+# https://docs.djangoproject.com/en/2.1/ref/forms/validation/
+# validation example clear !!
+# http://www.learningaboutelectronics.com/Articles/How-to-create-a-custom-field-validator-in-Django.php
+# overlap?
+# Key lookups?
+
+        else:
+            super().save(*args, **kwargs)  
 
 
--- penpal_app/urls.py
+++ penpal_app/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 
-# urlpatterns = [
-#     path('', views.user_index, name='user_index'),
-# ] 
+
 urlpatterns = [
     path('user/', views.user_index, name='user_index'),
     path('user/<int:id>', views.user_show, name='user_show'),
